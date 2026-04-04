aa9ade249f49438ef983f4260acdceb5a032212d
Jane Akinyi Nyasoro <janenyasoro@gmail.com>
2026-04-04 06:38:17 +0300

    using arrays and functions in JS

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,33 @@
 // Write your code here
 
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+//function to log the details of the first product in the array
+ function logFirstProduct(){
+  console.log(products[0]);
+ }
+
+ //function to add new product to the array
+ function addProduct(productName){
+  products.push(productName);
+ }
+
+ //
+ function updateProductName(position, newName){
+  if (position < 0){
+    position = products.length + position;
+  }
+  if (position >= 0 && position < products.length){
+    products[position] = newName;
+  } else {
+    console.log("Invalid position");
+  }
+ }
+
+ //function to remove the last product
+ function removeLastProduct() {
+    return products.pop();
+}
 
 
 // Export the necessary parts for testing
